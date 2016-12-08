d6621604be4e6055165702ed21702ced7fdcfa97
jiangwest <jiangdm@aliyun.com>
2016-12-08 09:06:47 +0800

    how to use Git for window? I have some issue

---
+++ HelloGradle.java
@@ -0,0 +1,11 @@
+//package net.petrikainulainen.gradle;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class HelloGradle {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, jdm, Welcome to Gradle!");
+    }
+}
